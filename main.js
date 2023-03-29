71d10fb25eaf6aa5c32159e7a14fb7f8fdbf6563
PopovD <dimdimovich95@gmail.com>
2023-03-29 20:43:18 +0300

    Google maps key wrapping

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const apiKey = "YOUR_API_KEY";
+
 function initMap() {
     const center = { lat: -34.397, lng: 150.644 };
     const map = new google.maps.Map(document.getElementById("map-container"), {
@@ -14,4 +16,12 @@ function initMap() {
     // Здесь можно добавить маркеры для ближайших пляжей с помощью цикла или других методов
 }
 
-window.addEventListener("load", initMap);
+//window.addEventListener("load", initMap);
+
+// Замените эту строку на следующую:
+// <script async defer src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"></script>
+const script = document.createElement("script");
+script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
+script.async = true;
+script.defer = true;
+document.body.appendChild(script);
