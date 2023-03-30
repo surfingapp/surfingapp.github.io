4050e4029fcb13e6e33027850bd45697d317c334
PopovD <dimdimovich95@gmail.com>
2023-03-30 13:21:28 +0300

    Made the code object-oriented

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
-const apiKey = "YOUR_API_KEY";
+class SurfingMap {
+  constructor(apiKey) {
+    this.apiKey = apiKey;
+    this.initMap();
+  }
 
-function initMap() {
+  initMap() {
     const center = { lat: -34.397, lng: 150.644 };
     const map = new google.maps.Map(document.getElementById("map-container"), {
-        zoom: 10,
-        center: center,
+      zoom: 10,
+      center: center,
     });
 
     const userMarker = new google.maps.Marker({
-        position: center,
-        map: map,
-        title: "Ваше местоположение",
+      position: center,
+      map: map,
+      title: "Ваше местоположение",
     });
 
     // Здесь можно добавить маркеры для ближайших пляжей с помощью цикла или других методов
+  }
+
+  loadScript() {
+    const script = document.createElement("script");
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${this.apiKey}&callback=initMap`;
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+  }
 }
 
-//window.addEventListener("load", initMap);
-
-// Замените эту строку на следующую:
-// <script async defer src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"></script>
-const script = document.createElement("script");
-script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
-script.async = true;
-script.defer = true;
-document.body.appendChild(script);
+const apiKey = "YOUR_API_KEY";
+const surfingMap = new SurfingMap(apiKey);
+surfingMap.loadScript();
