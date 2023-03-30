3b99ec5e25fb3b46553ffcd4636551e2a7940140
PopovD <dimdimovich95@gmail.com>
2023-03-30 21:10:08 +0300

    TinaCMS update

---
+++ _site/tina/config.js
@@ -0,0 +1,45 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "dbe1df1d-395e-40c6-9f97-0a8bbb6470be", // Get this from tina.io
+  token: "388b7feb5477896e2261cd7d3f56a35f477433c6", // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "./",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "./",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
