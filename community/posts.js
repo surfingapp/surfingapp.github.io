03ee681d48780018167d93e4ea2701491c14c3fb
PopovD <dimdimovich95@gmail.com>
2023-04-09 12:20:53 +0300

    Blog posts preview updates

--- community/posts.js
+++ community/posts.js
@@ -1,53 +1,77 @@
-async function fetchPosts(jsonPath) {
-  const response = await fetch(jsonPath);
-  const posts = await response.json();
-  return posts;
-}
-
-
-async function displayLatestPosts(containerSelector, folderPath, count = 3) {
-  const container = document.querySelector(containerSelector);
-  const posts = await fetchPosts(folderPath);
-
-  for (let i = 0; i < Math.min(count, posts.length); i++) {
-    const post = posts[i];
-    const postDiv = document.createElement('div');
-    postDiv.classList.add('post-preview');
-
-    const postTitle = document.createElement('h3');
-    postTitle.textContent = post.title;
-    postDiv.appendChild(postTitle);
-
-    const postExcerpt = document.createElement('p');
-    postExcerpt.textContent = post.excerpt;
-    postDiv.appendChild(postExcerpt);
-
-    const postLink = document.createElement('a');
-    postLink.href = post.link;
-    postLink.textContent = 'Read more';
-    postDiv.appendChild(postLink);
-
-    container.appendChild(postDiv);
+class Blog {
+  constructor(jsonPath) {
+    this.jsonPath = jsonPath;
   }
-}
 
-async function displayAllPosts(containerSelector, folderPath) {
-  const container = document.querySelector(containerSelector);
-  const posts = await fetchPosts(folderPath);
+  async fetchPosts() {
+    const response = await fetch(this.jsonPath);
+    const posts = await response.json();
+    return posts;
+  }
 
-  for (const post of posts) {
+  async fetchPostContent(postLink) {
+    const response = await fetch(postLink);
+    const html = await response.text();
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+
+    const title = doc.querySelector('h2').textContent;
+    const image = doc.querySelector('img');
+    const imageUrl = image ? image.src : null;
+    const content = Array.from(doc.querySelectorAll('p')).map((p) => p.textContent);
+
+    return { title, imageUrl, content };
+  }
+
+  async displayLatestPosts(containerSelector, count = 3) {
+    const container = document.querySelector(containerSelector);
+    const posts = await this.fetchPosts();
+
+    for (let i = 0; i < Math.min(count, posts.length); i++) {
+      const post = posts[i];
+      const { title, imageUrl, content } = await this.fetchPostContent(post.link);
+      const postDiv = this.createPostDiv(title, imageUrl, content, post.link, 1);
+      container.appendChild(postDiv);
+    }
+  }
+
+  async displayAllPosts(containerSelector) {
+    const container = document.querySelector(containerSelector);
+    const posts = await this.fetchPosts();
+
+    for (const post of posts) {
+      const { title, imageUrl, content } = await this.fetchPostContent(post.link);
+      const postDiv = this.createPostDiv(title, imageUrl, content, post.link, 4);
+      container.appendChild(postDiv);
+    }
+  }
+
+  createPostDiv(title, imageUrl, content, link, contentParagraphs) {
     const postDiv = document.createElement('div');
     postDiv.classList.add('post-preview');
 
     const postTitle = document.createElement('h3');
-    postTitle.textContent = post.title;
+    postTitle.textContent = title;
     postDiv.appendChild(postTitle);
 
+    if (imageUrl) {
+      const postImage = document.createElement('img');
+      postImage.src = imageUrl;
+      postImage.alt = title;
+      postDiv.appendChild(postImage);
+    }
+
+    for (let i = 0; i < contentParagraphs && i < content.length; i++) {
+      const postExcerpt = document.createElement('p');
+      postExcerpt.textContent = content[i];
+      postDiv.appendChild(postExcerpt);
+    }
+
     const postLink = document.createElement('a');
-    postLink.href = post.link;
+    postLink.href = link;
     postLink.textContent = 'Read more';
     postDiv.appendChild(postLink);
 
-    container.appendChild(postDiv);
+    return postDiv;
   }
 }
